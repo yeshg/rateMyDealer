f38cd1b920ecb504cab68f4a50095149ea162e48
Yesh Godse <yesh.godse@gmail.com>
2019-05-30 13:42:18 -0700

    added basic barebones app template

---
+++ app.js
@@ -0,0 +1,54 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+// For MongoDB
+var monk = require('monk');
+var db = monk('localhost:27017/nodetest1');
+
+// create index and user routes
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+// instantiate express, assign app variable to it
+var app = express();
+
+// view engine setup (where to find views)
+app.set('views', path.join(__dirname, 'views'));  // serve views from /views/
+app.set('view engine', 'ejs');                    // use EJS engine to render views
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public'))); // serve static objects from /public/
+
+// Make our db accessible to our router
+app.use(function(req,res,next){
+  req.db = db;
+  next();
+});
+
+// requests to http://localhost:3000/<arg1> should route to <arg2>
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ structure.md
@@ -0,0 +1,27 @@
+Home page: A bunch of cards of users
+- can pick user
+- can create new user
+
+Create User page:
+- put in your schedule (reuse create/delete events)
+- list of friends
+
+User page:
+- schedule shows
+- can create, delete events in schedule
+- can create hangout
+
+Hangout page
+- choose friends to hangout with
+    - app will suggest times for you to hangout
+
+
+
+HTML - Bootstrap
+CSS  - Bootstrap
+JS   - Angular? 
+Node.js - express
+MongoDB
+
+
+
